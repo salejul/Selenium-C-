4a52c948b6999efdfa4d2c9aba6bc9aade6dc2d9
Aleksandar Julovski <aleksandar.julovski83@gmail.com>
2019-12-27 17:12:37 +0100

    First change

---
+++ MyFramework/SeleniumPracticeClass.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace MyFramework
+{
+    [TestFixture]
+    class SeleniumPracticeClass : SeleniumBaseClass
+    {
+
+        [Test]
+        [Author("Aleksandar")]
+        [Category("FirstFlight")]
+        public void TodVachevDragAndDrop()
+        {
+
+            this.NavigateTo("http://testing.todvachev.com/draganddrop/draganddrop.html");
+            IWebElement draggable = this.FindElement(By.CssSelector("#Drag1"));
+            IWebElement droppable = this.FindElement(By.CssSelector("#Drag2"));
+
+            Actions builder = new Actions(this.Driver);
+
+            builder.ClickAndHold(draggable).MoveToElement(droppable).MoveByOffset(0, 10).Release().Build().Perform();
+
+
+            DoWait(4);
+        }
+
+
+
+        [Test]
+        [Author("Aleksandar")]
+        [Category("FirstFlight")]
+        public void HerokuAddRemoveElements()
+        {
+            this.NavigateTo("http://the-internet.herokuapp.com/add_remove_elements/");
+            this.FindElement(By.XPath("//div[@class='example']/button[starts-with(text(), 'Add')]"))?.Click();
+            this.FindElement(By.XPath("//div[@class='example']/button[starts-with(text(), 'Add')]"))?.Click();
+            this.FindElement(By.XPath("//div[@class='example']/button[starts-with(text(), 'Add')]"))?.Click();
+            var elementsToClick = this.FindElements(By.XPath("//div[@id='elements']//button"));
+            foreach (var element in elementsToClick)
+            {
+                element.Click();
+                //DoWait(2);
+            }
+            //DoWait(2);
+        }
+
+        [Test]
+        [Author("Aleksandar")]
+        [Category("FirstFlight")]
+        public void HerokuAlerts() {
+
+            this.NavigateTo("http://the-internet.herokuapp.com/javascript_alerts");
+
+            //*************************
+            //ClickForJsAlertButtonTest
+            //*************************
+            this.FindElement(By.XPath("//div[1]/ul[1]/li[1]/button[1]"))?.Click();
+            IAlert alert1 = this.Driver.SwitchTo().Alert();
+            DoWait(2);
+            alert1.Accept();
+            string alertResult = this.FindElement(By.XPath("//p[@id='result']")).Text;
+            Assert.AreEqual(alertResult, "You successfuly clicked an alert", "Something went wrong!");
+
+            //***********************
+            //ClickForJsConfirmOKTest
+            //***********************
+            this.FindElement(By.XPath("//div[1]/ul[1]/li[2]/button[1]"))?.Click();
+            IAlert alert2 = this.Driver.SwitchTo().Alert();
+            DoWait(1);
+            alert2.Accept();
+            string confirmOKResult = this.FindElement(By.XPath("//p[@id='result']")).Text;
+            Assert.AreEqual(confirmOKResult, "You clicked: Ok", "Something went wrong!");
+
+            //***************************
+            //ClickForJsConfirmCancelTest
+            //***************************
+            this.FindElement(By.XPath("//div[1]/ul[1]/li[2]/button[1]"))?.Click();
+            IAlert alert3 = this.Driver.SwitchTo().Alert();
+            DoWait(1);
+            alert3.Dismiss();
+            string confirmCancelResult = this.FindElement(By.XPath("//p[@id='result']")).Text;
+            Assert.AreEqual(confirmCancelResult, "You clicked: Cancel", "Something went wrong!");
+
+            //********************
+            //ClickForJsPromptTest
+            //********************
+            this.FindElement(By.XPath("//div[1]/ul[1]/li[3]/button[1]"))?.Click();
+            IAlert alert4 = this.Driver.SwitchTo().Alert();
+            string keysToSend = "Hello there folks!";
+            alert4.SendKeys(keysToSend);
+            DoWait(4);
+            alert4.Accept();
+            string confirmKeysToSendResult = this.FindElement(By.XPath("//p[@id='result']")).Text;
+            Assert.AreEqual(confirmKeysToSendResult, $"You entered: {keysToSend}", "Something went wrong!");
+        }
+
+        [Test]
+        [Author("Aleksandar")]
+        [Category("FirstFlight")]
+        public void HerokuTypos()
+        {
+            this.NavigateTo("http://the-internet.herokuapp.com/typos");
+            string paragraphToCkeck = this.FindElement(By.XPath("//div[1]/p[2]")).Text;
+
+            if (paragraphToCkeck.Contains("won,t"))
+                Assert.Fail();
+            else
+                Assert.Pass();
+        }
+
+        [Test]
+        [Author("Aleksandar")]
+        [Category("FirstFlight")]
+        public void HerokuWindows() {
+
+            this.NavigateTo("http://the-internet.herokuapp.com/windows");
+            IWebElement dugmence = this.FindElement(By.XPath("//div[@class='example']/a"));
+            //Console.WriteLine(dugmence.Displayed);
+            //Console.WriteLine(dugmence.Enabled);
+            //Console.WriteLine(dugmence.Size);
+            //Console.WriteLine(dugmence.Text);
+            //Console.WriteLine(dugmence.Location);
+            //Console.WriteLine(dugmence.TagName);
+
+            dugmence.Click();
+            DoWait(1);
+            var popup = this.Driver.WindowHandles[1];
+            this.Driver.SwitchTo().Window(popup);
+            DoWait(3);
+            this.Driver.Close(); // Closes popup window
+            DoWait(2);
+            this.Driver.SwitchTo().Window(this.Driver.WindowHandles[0]);
+
+        }
+
+        [Test]
+        [Author("Aleksandar")]
+        [Category("FirstFlight")]
+        public void SeleniumEasyMultiPopup()
+        {
+
+            this.NavigateTo("https://www.seleniumeasy.com/test/window-popup-modal-demo.html");
+            IWebElement dugmence = this.FindElement(By.CssSelector("#followall"));
+            dugmence.Click();
+            DoWait(1);
+            var popup3 = this.Driver.WindowHandles[3];
+            this.Driver.SwitchTo().Window(popup3);
+            DoWait(3);
+            this.Driver.Close(); // Closes popup window
+            DoWait(2);
+            var popup2 = this.Driver.WindowHandles[2];
+            this.Driver.SwitchTo().Window(popup2);
+            DoWait(3);
+            this.Driver.Close(); // Closes popup window
+            DoWait(2);
+            var popup1 = this.Driver.WindowHandles[1];
+            this.Driver.SwitchTo().Window(popup1);
+            DoWait(3);
+            this.Driver.Close(); // Closes popup window
+            this.Driver.SwitchTo().Window(this.Driver.WindowHandles[0]);
+
+        }
+
+        [Test]
+        [Author("Aleksandar")]
+        [Category("FirstFlight")]
+        public void TwoInputFields() {
+
+            this.NavigateTo("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+            string keysToSend1 = "45";
+            string keystoSend2 = "89";
+            int result = Convert.ToInt32(keysToSend1) + Convert.ToInt32(keystoSend2);
+
+            this.FindElement(By.CssSelector("#sum1"))?.SendKeys(keysToSend1);
+            this.FindElement(By.CssSelector("#sum2"))?.SendKeys(keystoSend2);
+
+            DoWait(4);
+
+            //check if equal
+            this.FindElement(By.XPath("//*[@id='gettotal']/button"))?.Click();
+            DoWait(5);
+            Assert.AreEqual($"Total a + b ={result}", $"Total a + b ={this.FindElement(By.CssSelector("#displayvalue")).Text}", "Something went wrong!");
+
+        }
+
+        [Test]
+        [Author("Aleksandar")]
+        [Category("FirstFlight")]
+        public void RegistracijaQATodorovvv() {
+
+            this.NavigateTo("http://qa.todorowww.net/register");
+            this.FindElement(By.XPath("//input[@name='ime']"))?.SendKeys("Milovan");
+            this.FindElement(By.XPath("//input[@name='prezime']"))?.SendKeys("Milovanovic");
+            this.FindElement(By.XPath("//input[@name='korisnicko']"))?.SendKeys("MMTuts");
+            this.FindElement(By.Id("password"))?.SendKeys("12345");
+            this.FindElement(By.Id("passwordAgain"))?.SendKeys("12345");
+            this.FindElement(By.XPath("//input[@id='pol_m']"))?.Click();
+            var volim = this.FindElements(By.XPath("//input[@name='volim[]']"));
+            foreach (var element in volim)
+            {
+                element.Click();
+            }
+            this.FindElement(By.XPath("//input[@id='passwordOptional']"))?.SendKeys("54321");
+            this.FindElement(By.XPath("//input[@name='grad']"))?.SendKeys("NIS");
+            IWebElement selektujZemlju = this.FindElement(By.Name("zemlja"));
+            var select = new SelectElement(selektujZemlju);
+            select.SelectByText("Makedonija");
+            this.FindElement(By.CssSelector("#eyecolor"))?.SendKeys("plava");
+            this.FindElement(By.CssSelector("#haircolor"))?.SendKeys("crna");
+            IWebElement selektujVisinu = this.FindElement(By.Name("visina"));
+            var select1 = new SelectElement(selektujVisinu);
+            select1.SelectByValue("180-190");
+            IWebElement selektujTezinu = this.FindElement(By.Name("tezina"));
+            var select2 = new SelectElement(selektujTezinu);
+            select2.SelectByValue("80-100");
+            this.FindElement(By.XPath("//textarea[@id='intro']"))?.SendKeys("Ovo je kratak uvod o meni");
+            this.FindElement(By.XPath("//textarea[@id='aboutme']"))?.SendKeys("Par recenica o meni.");
+            this.FindElement(By.XPath("//input[@name='contactmodern' and @value='sms']"))?.Click();
+            IWebElement selektujDestinaciju = this.FindElement(By.Name("summer"));
+            var select3 = new SelectElement(selektujDestinaciju);
+            select3.SelectByValue("usa-fl");
+            DoWait(4);
+            this.FindElement(By.XPath("//input[@name='register']"))?.Submit();
+
+            string successMessage = this.FindElement(By.XPath("//div[@class='alert alert-success']")).Text;
+
+            if (successMessage.Contains("Uspešno"))
+                Assert.Pass("Success!");
+            else
+                Assert.Fail("Failure!!!!!");
+
+        }
+
+        [Test]
+        [Category("Cas34")]
+        [Author("Aleksandar")]
+        public void EmmiTest() {
+
+            this.NavigateTo("https://www.emmi.rs/");
+            this.FindElement(By.XPath("//a[@title='Monitori']"))?.Click();
+            IWebElement proizvodjac = this.FindElement(By.XPath("//select[@name='brandId']"));
+            var selektujProizvodjaca = new SelectElement(proizvodjac);
+            selektujProizvodjaca.SelectByValue("448");
+            IWebElement tip = this.FindElement(By.XPath("//select[@name='tip']"));
+            var selektujTip = new SelectElement(tip);
+            selektujTip.SelectByValue("34878");
+            this.FindElement(By.XPath("//input[@class='categoriesListButton']"))?.Click();
+
+            this.FindElement(By.XPath("//div[5]/div[1]/div[2]/div[1]//a[contains(text(),'HP OMEN')]"))?.Click();
+
+            IWebElement price = this.FindElement(By.XPath("//div[@class='price']"));
+
+            Console.WriteLine(price.Text);
+
+            string actualPrice = price.Text.Trim();
+
+            Console.WriteLine(actualPrice);
+
+
+            if (actualPrice == "29.990")
+                Assert.Pass("Test Prosao");
+            else
+                Assert.Fail("Test nije prosao");
+
+        }
+
+        [Test]
+        [Category("Cas34")]
+        [Author("Aleksandar")]
+        public void OrderYourBugsTodaySMALL() {
+
+            //sad path test should fail
+            this.NavigateTo("http://shop.qa.rs/");
+
+            IWebElement order = this.FindElement(By.CssSelector("div.container:nth-child(2) div.row:nth-child(8) div.col-sm-3.text-center:nth-child(3) div.panel.panel-warning div.panel-body.text-justify form:nth-child(1) p.text-center:nth-child(4) > select:nth-child(2)"));
+            var chooseQuantity = new SelectElement(order);
+            chooseQuantity.SelectByValue("6");
+            DoWait(2);
+            int itemPrice = Convert.ToInt32(this.FindElement(By.XPath("//h3[contains(text(),'SMALL')]/parent::div/following-sibling::div[2]")).Text.Substring(1));
+            this.FindElement(By.CssSelector("div.container:nth-child(2) div.row:nth-child(8) div.col-sm-3.text-center:nth-child(3) div.panel.panel-warning div.panel-body.text-justify form:nth-child(1) p.text-center.margin-top-20:nth-child(5) > input.btn.btn-primary"))?.Click();
+            DoWait(5);
+
+            int quantity = Convert.ToInt32(this.FindElement(By.XPath("//table[1]/tbody[1]/tr[1]/td[2]")).Text);
+            int pricePerItem = Convert.ToInt32(this.FindElement(By.XPath("//table[1]/tbody[1]/tr[1]/td[3]")).Text.Substring(1));
+            int totalItemPrice = Convert.ToInt32(this.FindElement(By.XPath("//table[1]/tbody[1]/tr[1]/td[4]")).Text.Substring(1));
+            int shipping = Convert.ToInt32(this.FindElement(By.XPath("//table[1]/tbody[1]/tr[2]/td[3]")).Text.Trim().Substring(1));
+            int total = Convert.ToInt32(this.FindElement(By.XPath("//table[1]/tbody[1]/tr[3]/td[1]")).Text.Substring(8));
+
+            int expectedValue = (quantity * itemPrice) + shipping;
+            int actualValue = (quantity * pricePerItem) + shipping;
+
+            if (expectedValue == total)
+                Assert.Pass("Test Passed!");
+            else
+                Assert.Fail($"Test Failed! Value is {actualValue}$ but should be {expectedValue}$.");
+             
+
+            //Console.WriteLine(itemPrice);
+
+
+        }
+
+        [Test]
+        [Category("Cas34")]
+        [Author("Aleksandar")]
+        public void OrderYourBugsTodayENTERPRISE()
+        {
+
+            //sad path test should fail
+            this.NavigateTo("http://shop.qa.rs/");
+
+            IWebElement order = this.FindElement(By.CssSelector("div.container:nth-child(2) div.row:nth-child(8) div.col-sm-3.text-center:nth-child(5) div.panel.panel-success div.panel-body.text-justify form:nth-child(1) p.text-center:nth-child(4) > select:nth-child(2)"));
+            var chooseQuantity = new SelectElement(order);
+            chooseQuantity.SelectByValue("6");
+            DoWait(2);
+            int itemPrice = Convert.ToInt32(this.FindElement(By.XPath("//h3[contains(text(),'ENTERPRISE')]/parent::div/following-sibling::div[2]")).Text.Remove(4,1));
+            Console.WriteLine(itemPrice);
+            this.FindElement(By.CssSelector("div.container:nth-child(2) div.row:nth-child(8) div.col-sm-3.text-center:nth-child(5) div.panel.panel-success div.panel-body.text-justify form:nth-child(1) p.text-center.margin-top-20:nth-child(5) > input.btn.btn-default"))?.Click();
+            DoWait(5);
+
+            //TrimEnd('','!').Trim()
+
+            int quantity = Convert.ToInt32(this.FindElement(By.XPath("//table[1]/tbody[1]/tr[1]/td[2]")).Text);
+            int pricePerItem = Convert.ToInt32(this.FindElement(By.XPath("//table[1]/tbody[1]/tr[1]/td[3]")).Text.Substring(1));
+            int totalItemPrice = Convert.ToInt32(this.FindElement(By.XPath("//table[1]/tbody[1]/tr[1]/td[4]")).Text.Substring(1));
+            int shipping = Convert.ToInt32(this.FindElement(By.XPath("//table[1]/tbody[1]/tr[2]/td[3]")).Text.Trim().Substring(1));
+            int total = Convert.ToInt32(this.FindElement(By.XPath("//table[1]/tbody[1]/tr[3]/td[1]")).Text.Substring(8));
+
+            int expectedValue = (quantity * itemPrice) + shipping;
+            int actualValue = (quantity * pricePerItem) + shipping;
+
+            if (expectedValue == total)
+                Assert.Pass("Test Passed!");
+            else
+                Assert.Fail($"Test Failed! Value is {actualValue}$ but should be {expectedValue}$.");
+
+
+            //Console.WriteLine(itemPrice);
+
+
+        }
+
+        [SetUp]
+        public void SetUpTests()
+        {
+            //this.Driver = new FirefoxDriver();
+            this.Driver = new ChromeDriver();
+            this.Driver.Manage().Window.Maximize();
+            this.Wait = 3;
+        }
+
+        [TearDown]
+        public void TearDownTests()
+        {
+            this.Close();
+        }
+    }
+}
---
+++ MyFramework/Tests.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace MyFramework
+{
+    class Tests : SeleniumBaseClass
+    {
+        private readonly FileManagement log = new FileManagement();
+
+        public Tests()
+        {
+            this.log.FileName = @"C:\Test\logger.txt";
+        }
+
+        [Test]
+        [Category("First Class")]
+        public void PerformGoogleSearch()
+        {
+            //this.log.Store("*** PerformGoogleSearch ***");
+            this.NavigateTo("http://www.google.rs/");
+            IWebElement el = this.FindElement(By.Name("q"));
+            try
+            {
+                this.SendKeys("Selenium C# Helpers", true, el);
+            }
+            catch (Exception e)
+            {
+                this.log.Store($"Exception <{e.Message}>");
+                Assert.Fail("Couldn't send keys, element not defined.");
+                return;
+            }
+            Assert.Pass("Keys sent");
+            this.DoWait(2);
+        }
+
+        [Test]
+        [Category("First Class")]
+        public void SelectHawaii()
+        {
+            string selectByText = "Havaji, SAD";
+            //this.log.Store("*** SelectHawaii ***");
+            this.NavigateTo("http://qa.todorowww.net/register");
+            IWebElement el = this.FindElement(By.Name("summer"));
+            var select = new SelectElement(el);
+            select.SelectByText(selectByText);
+            this.DoWait(2);
+            string selectedItem = select.SelectedOption.Text;
+            Assert.AreEqual(selectedItem, selectByText, "Selected item doesn't match required.");
+        }
+
+        [Test]
+        [Category("First Class")]
+        public void ClickOnRadioButtons()
+        {
+            //this.log.Store("*** ClickOnRadioButtons ***");
+            this.NavigateTo("http://qa.todorowww.net/register");
+            var elements = this.FindElements(By.XPath("//input[@type='radio']"));
+            foreach (var el in elements)
+            {
+                el.Click();
+                this.DoWait(1);
+            }
+            this.DoWait(1);
+            IWebElement requiredElement = this.FindElement(By.XPath("//input[@type='radio' and @name='contactmodern' and @value='sms']"));
+            string isChecked = requiredElement.GetAttribute("checked");
+            if (isChecked == null)
+            {
+                Assert.Fail("Required radio button is not checked");
+            }
+            else
+            {
+                Assert.True(isChecked.ToLower().Equals("true"), "Something went wrong.");
+            }
+        }
+
+        [Test]
+        [Category("First Class")]
+        public void CheckAllVolimCheckboxes()
+        {
+            //this.log.Store("*** CheckAllVolimCheckboxes ***");
+            this.NavigateTo("http://qa.todorowww.net/register");
+            var elements = this.FindElements(By.Name("volim[]"));
+            foreach (var el in elements)
+            {
+                el.Click();
+                this.DoWait(1);
+            }
+            this.DoWait(1);
+            IWebElement requiredElement = this.FindElement(By.XPath("//input[@name='volim[]' and @value='paradajz']"));
+            Assert.True(requiredElement.Selected, "Required checkbox is not checked");
+            this.log.Store($"{requiredElement.Selected} is really selected");
+        }
+
+        [Test]
+        [Category("First Class")]
+        public void ExtractYearsFromTable()
+        {
+            this.log.Store("*** ExtractYearsFromTable ***");
+            this.NavigateTo("https://www.toolsqa.com/automation-practice-table/");
+            IWebElement table = this.FindElement(By.ClassName("tsc_table_s13"));
+            var columns = FindElements(By.XPath("//tbody/tr/td[4]"), table);
+            List<string> expectedYears = new List<string>(new string[] { "2010", "2012", "2004", "2008" });
+            bool allPass = true;
+            int counter = 0;
+            foreach (var col in columns)
+            {
+                if (expectedYears[counter] != col.Text)
+                {
+                    this.log.Store($"expectedYears[{counter}] = {expectedYears[counter]}, got {col.Text}");
+                    allPass = false;
+                    break;
+                }
+                else
+                {
+                    this.log.Store("All good.");
+                }
+                counter++;
+            }
+            Assert.True(allPass, "Some of the values are not the same.");
+        }
+
+        [Test]
+        [Category("Cas32")]
+        public void TestDragAndDrop()
+        {
+            // Doesn't work
+            this.NavigateTo("https://formy-project.herokuapp.com/dragdrop");
+            IWebElement target = this.FindElement(By.CssSelector("#box"));
+            IWebElement drop1 = this.FindElement(By.Id("image"));
+            var action = new Actions(this.Driver);
+            
+            action.ClickAndHold(drop1);
+            action.MoveToElement(target);
+            action.MoveByOffset(-15, 274);
+            action.Release(target);
+            action.Build();
+            action.Perform();
+
+
+            //IWebElement image = this.FindElement(By.Id("image"));
+            //IWebElement target = this.FindElement(By.Id("box"));
+            //Actions builder1 = new Actions(this.Driver);
+            //IAction dragAndDrop1 = builder1.ClickAndHold(image).MoveToElement(target).Release(target).Build();
+            //dragAndDrop1.Perform();
+
+
+            DoWait(2);
+        }
+
+        [Test]
+        [Category("Cas33")]
+
+        public void TestVezba()
+        {
+            
+            this.NavigateTo("http://test.qa.rs");
+            var findBy = By.XPath("//a[contains(text(),'Kreiraj novog korisnika')]");
+            var wait = new WebDriverWait(this.Driver, TimeSpan.FromSeconds(20));
+            wait.Until(drv => drv.FindElement(findBy));
+            IWebElement link = this.FindElement(findBy);
+            link.Click();
+
+            IWebElement ime = this.FindElement(By.XPath("//input[@name='ime']"));
+            ime.SendKeys("Marko");
+
+            IWebElement prezime = this.FindElement(By.XPath("//input[@name='prezime']"));
+            prezime.SendKeys("Markovic");
+
+            IWebElement korisnicko = this.FindElement(By.XPath("//input[@name='korisnicko']"));
+            korisnicko.SendKeys("MMarkovic");
+
+            IWebElement email = this.FindElement(By.XPath("//input[@name='email']"));
+            email.SendKeys("markovic@gmail.com");
+
+            IWebElement telefon = this.FindElement(By.XPath("//input[@name='telefon']"));
+            telefon.SendKeys("06111001100");
+
+            IWebElement izaberiZemlju = this.FindElement(By.Name("zemlja"));
+            var select = new SelectElement(izaberiZemlju);
+            select.SelectByValue("srb");
+
+            var findBy2 = By.XPath("//select[@id='grad']");
+            var wait2 = new WebDriverWait(this.Driver, TimeSpan.FromSeconds(20));
+            wait2.Until(drv => drv.FindElement(findBy2));
+
+            IWebElement izaberiGrad = this.FindElement(findBy2);
+            var selectgrad = new SelectElement(izaberiGrad);
+            selectgrad.SelectByValue("Niš");
+
+            IWebElement ulica = this.FindElement(By.XPath("//form[1]/div[8]/div[2]/input[1]"));
+            ulica.SendKeys("Neznanog Junaka 2");
+
+            IWebElement pol = this.FindElement(By.XPath("//input[@id='pol_m']"));
+            pol.Click();
+
+            IWebElement cekbox1 = this.FindElement(By.Id("newsletter"));
+            cekbox1.Click();
+
+            IWebElement cekbox2 = this.FindElement(By.Id("promotions"));
+            cekbox2.Click();
+
+            IWebElement button = this.FindElement(By.XPath("//input[@name='register']"));
+            button.Submit();
+
+
+        }
+
+        [Test]
+        [Category("Cas33")]
+        public void ShoppingCart()
+        {
+
+            this.NavigateTo("http://shop.qa.rs/");
+
+            IWebElement vrednost = this.FindElement(By.CssSelector("div.container:nth-child(2) div.row:nth-child(8) div.col-sm-3.text-center:nth-child(4) div.panel.panel-info div.panel-body.text-justify form:nth-child(1) p.text-center:nth-child(4) > select:nth-child(2)"));
+            var desetka = new SelectElement(vrednost);
+            desetka.SelectByValue("10");
+
+            IWebElement order = this.FindElement(By.CssSelector("div.container:nth-child(2) div.row:nth-child(8) div.col-sm-3.text-center:nth-child(4) div.panel.panel-info div.panel-body.text-justify form:nth-child(1) p.text-center.margin-top-20:nth-child(5) > input.btn.btn-primary"));
+            order.Click();
+
+            int quantity = Convert.ToInt32((this.FindElement(By.XPath("//tr[1]//td[2]")).Text));
+            int pricePerItem = Convert.ToInt32(this.FindElement(By.XPath("//tr[1]//td[3]")).Text.Substring(1));
+
+
+            Assert.AreEqual(quantity, 10, "Desired quantity doesn't match required citeria");
+            Assert.AreEqual(pricePerItem, 800, "Desired price per item doesn't match required citeria");
+
+            int totalItemPriceInt = quantity * pricePerItem;
+            int totalPricePerItem = Convert.ToInt32(this.FindElement(By.XPath("//tr[3]//td[1]")).Text.Substring(8));
+
+            DoWait(4);
+
+            if (totalItemPriceInt != totalPricePerItem)
+                Assert.Fail();
+            else
+                Assert.Pass();
+
+            
+
+        }
+
+
+
+
+        [SetUp]
+        public void SetUpTests()
+        {
+            this.Driver = new FirefoxDriver();
+            //this.Driver = new ChromeDriver();
+            this.Wait = 3;
+        }
+
+        [TearDown]
+        public void TearDownTests()
+        {
+            this.Close();
+        }
+    }
+}
